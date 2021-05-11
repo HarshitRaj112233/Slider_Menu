b810b3e8eead27b3044654060e82ae5cc99b65df
Harshit Raj <rajharshitraj112233@gmail.com>
2021-05-11 16:34:33 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+var navigation = document.querySelector(".navigation")
+document.querySelector(".toggle").onclick = function () {
+    this.classList.toggle("active")
+    navigation.classList.toggle('active')
+}
